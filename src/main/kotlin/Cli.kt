96c19bc89507c5ed66c30e5e747577ca8d9a4183
Mr-Pine <git@mr-pine.de>
2024-01-05 02:19:41 +0100

    Improve cli

--- src/main/kotlin/Cli.kt
+++ src/main/kotlin/Cli.kt
@@ -1,9 +1,9 @@
 package de.mr_pine.doctex
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.context
+import com.github.ajalt.clikt.output.MordantHelpFormatter
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.arguments.default
-import com.github.ajalt.clikt.parameters.arguments.validate
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.enum
@@ -11,12 +11,18 @@ import com.github.ajalt.clikt.parameters.types.file
 import java.io.File
 
 class Cli : CliktCommand(name = "doctex") {
-    val sourcedir by argument().file(mustExist = true, canBeFile = false, mustBeReadable = true)
-    val rootPackage by argument()
-    val output by option().file(canBeDir = false, mustBeWritable = true).default(File("./documentation.tex"))
-    val minimumVisibility by argument().enum<Visibility>().default(Visibility.PROTECTED)
+    private val sourceDir by argument(help = "The directory containing all source files the documentation should be generated for").file(mustExist = true, canBeFile = false, mustBeReadable = true)
+    private val rootPackage by argument(help = "The package containing all subpackages and classes the documentation should be generated for")
+    private val output by option().file(canBeDir = false, mustBeWritable = true).default(File("./documentation.tex"))
+    private val minimumVisibility by option().enum<Visibility>().default(Visibility.PROTECTED)
+
+    init {
+        context {
+            helpFormatter = { MordantHelpFormatter(it, showDefaultValues = true) }
+        }
+    }
     override fun run() {
-        val doctex = DocTeX(sourcedir)
+        val doctex = DocTeX(sourceDir)
         doctex.writeJavadoc(output, rootPackage, minimumVisibility)
     }
 }
