c2d9c4824bdb14a6a49f953e66edfab800bed941
JFronny <git@akbvopenfl1.hopto.org>
2025-01-04 12:00:02 +0100

    ci: accept build scan ToS via argument

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,8 +75,3 @@ publishing {
         }
     }
 }
-
-extensions.findByName("buildScan")?.withGroovyBuilder {
-    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
-    setProperty("termsOfServiceAgree", "yes")
-}
